8699a615d7c6adba2f3c266c281a3ac4737b1c4d
Obramenko Alexandr <obram22@gmail.com>
2017-12-11 17:50:47 +0200

    asdasd

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+	mobileNavbar ();
+});
+
+function mobileNavbar () {
+	$('#js-hamburger').click(function(){
+		$(this).toggleClass('open');
+		$('.js-navbar').toggleClass('open');
+	});
+}
